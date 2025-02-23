25566dd34a18b07adaf989183b3ad6588b579692
Vinay Singh <codewithvinaysingh@gmail.com>
2025-02-23 19:21:22 +0530

    Challenge 3 done

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,21 @@
 /**
  * Write your challenge solution here
  */
+const nameInput = document.getElementById('nameInput');
+const jobInput = document.getElementById('jobInput');
+const ageInput = document.getElementById('ageInput');
+const bioInput = document.getElementById('bioInput');
+
+const nameDisplay = document.getElementById('nameDisplay');
+const jobDisplay = document.getElementById('jobDisplay');
+const ageDisplay = document.getElementById('ageDisplay');
+const bioDisplay = document.getElementById('bioDisplay');
+
+function updateInput(e, element) {
+    element.innerText = e.target.value || 'Not provided'
+}
+
+nameInput.addEventListener('keyup', (e) => updateInput(e, nameDisplay));
+jobInput.addEventListener('keyup', (e) => updateInput(e, jobDisplay));
+ageInput.addEventListener('keyup', (e) => updateInput(e, ageDisplay));
+bioInput.addEventListener('keyup', (e) => updateInput(e, bioDisplay));
